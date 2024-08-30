b8bd6b374d6929ce7193ff84f42f05c711f808b6
ahmedx793 <ahmedx793@gmail.com>
2024-08-30 17:31:30 +0000

    disable eslint

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  eslint: {
+    ignoreDuringBuilds: true,
+
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+    
+  },
+};
 
 export default config;
