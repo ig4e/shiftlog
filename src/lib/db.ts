e73e92e0b63416ad9c1724bdfd0ed4798a573917
ahmedx793 <ahmedx793@gmail.com>
2024-09-05 07:28:35 +0000

    feat(frontend): added auth

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,17 +1,14 @@
-import { Shift } from '@prisma/client';
-import Dexie, { EntityTable } from 'dexie';
+import { Shift } from "@prisma/client";
+import Dexie, { EntityTable } from "dexie";
 
-export const db = new Dexie('shiftDBv2') as Dexie & {
-    shifts: EntityTable<
-      Shift,
-      'id' // primary key "id" (for the typings only)
-    >;
-  };;
-
-
-
+export const db = new Dexie("shiftDBv3") as Dexie & {
+  shifts: EntityTable<
+    Omit<Shift, "userId">,
+    "id" // primary key "id" (for the typings only)
+  >;
+};
 
 db.version(1).stores({
-  shifts: '++id, startedAt, endedAt, updatedAt' 
+  shifts: "++id, startedAt, endedAt, updatedAt",
+  auth: "++id, email"
 });
-
