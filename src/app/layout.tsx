a48dd882178d46f14577997bd6908171ba6c93fb
ahmedx793 <ahmedx793@gmail.com>
2024-09-05 13:21:02 +0000

    fixing the theme color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,9 +39,9 @@ export default function RootLayout({
           href="/favicon-16x16.png"
         />
         <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+        <meta name="msapplication-TileColor" content="#000000" />
+        <meta name="theme-color" content="#000000" />
       </head>
       <body className="container">
         <Header />
